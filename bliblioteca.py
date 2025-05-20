4df99cc5983fcdc186320fdf0af35ec5e84a936b
Pedrinho <DandoGrau@gmail.com>
2025-05-20 08:16:15 -0300

    initial commit

---
+++ bliblioteca.py
@@ -0,0 +1,8 @@
+class Bliblioteca:
+    def __init__(self, titulo:str, autor:str, ano_de_publicacao:int, status:str):
+        self.titulo = titulo
+        self.autor = autor
+        self.ano_de_publicacao = ano_de_publicacao
+        self.status = status
+        
+    
